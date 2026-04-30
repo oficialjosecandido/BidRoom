095c24577d4ea0726e922ecd6b5af0ee5d7b19cc
oficialjosecandido <62559408+oficialjosecandido@users.noreply.github.com>
2026-04-30 15:39:27 +0100

    Add auction duration option for 5 minutes in production environment
    - Introduced a new auction duration option labeled '5 minutes' with a corresponding value of 1/12 hours in the environment.prod.ts file.

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -4,6 +4,7 @@ export const environment = {
   bidroomFeeSellerRate: 0.005,
   bidroomFeeBuyerRate: 0.005,
   auctionDurations: [
+    { label: '5 minutes', hours: 1 / 12 },
     { label: '1 hour',   hours: 1  },
     { label: '7 hours',  hours: 7  },
     { label: '24 hours', hours: 24 },
